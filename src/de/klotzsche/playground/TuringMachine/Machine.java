dff7701df17d2acd2eb6dbeecae75fe366dfa5fb
Felix Klotzsche <felixklotzsche@gmail.com>
2014-06-13 11:20:51 +0200

    removed redundant code even further

--- src/de/klotzsche/playground/TuringMachine/Machine.java
+++ src/de/klotzsche/playground/TuringMachine/Machine.java
@@ -1,24 +1,35 @@
 package de.klotzsche.playground.TuringMachine;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by Felix Klotzsche on 13.06.2014.
  */
 public class Machine {
 
-    private ReadWriteHead rwh;
-    private WorkingTape wt;
-
+    private int currentValue = 0;
     private char[] sourceCode;
+    private Map<Character, Command> symbolTable;
 
     public Machine(char[] sourceCode) {
-        this.wt = new WorkingTape();
-        this.rwh = new ReadWriteHead(wt, sourceCode.length - 1);
-
         this.sourceCode = sourceCode;
+
+        this.symbolTable = new HashMap<>();
+
+        this.setupSymbolTable();
+    }
+
+    private void setupSymbolTable(){
+        this.symbolTable.put('>', () -> {/*does nothing currently*/});
+        this.symbolTable.put('<', () -> {/*does nothing currently*/ });
+        this.symbolTable.put('+', () -> currentValue += 1);
+        this.symbolTable.put('-', () -> currentValue -= 1);
+        this.symbolTable.put('#', () -> System.out.println((char) ('a' + currentValue)));
     }
 
     public void run() {
         for(char c : sourceCode)
-            rwh.getSymbolTable().get(c).runCommand();
+            symbolTable.get(c).runCommand();
     }
 }
