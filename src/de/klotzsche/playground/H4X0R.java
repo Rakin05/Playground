e251ee98737e440664347643971c5718745bfe2d
Felix Klotzsche <felixklotzsche@gmail.com>
2014-06-13 08:59:25 +0200

    changed for loop to a nice IntStream...

--- src/de/klotzsche/playground/H4X0R.java
+++ src/de/klotzsche/playground/H4X0R.java
@@ -1,15 +1,14 @@
 package de.klotzsche.playground;
 
+import java.util.stream.IntStream;
+
 /**
  * Created by Felix Klotzsche on 12.06.2014.
  */
 public class H4X0R {
 
     public static void main(String[] args) {
-        for(int i = 42; i <= 999; i++)
-            if(checkPasscode(i)) System.out.println(i);
-
-
+        IntStream.range(42, 100).filter(i -> checkPasscode(i)).forEach(System.out::println);
     }
 
     public static boolean checkPasscode(int H4X0R) {
