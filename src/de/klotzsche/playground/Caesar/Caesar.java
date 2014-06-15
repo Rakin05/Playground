367c51a9b57aa7a25a2791ba8a087f3e61d72e29
Felix Klotzsche <felixklotzsche@gmail.com>
2014-06-15 13:06:00 +0200

    refactored filtering by Charactor or Whitespace

--- src/de/klotzsche/playground/Caesar/Caesar.java
+++ src/de/klotzsche/playground/Caesar/Caesar.java
@@ -3,37 +3,29 @@ package de.klotzsche.playground.Caesar;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.joining;
-import static java.util.stream.Collectors.toList;
 
 /**
  * Created by Felix on 14.06.2014.
  */
 public class Caesar {
 
-    public static String removeAllNonAlphabetics(String s){
-        return s.replaceAll("[^A-Za-z0-9üöäß ]","");
-    }
-
-    public static String cipherByPassword(String s, int amount){
-        return s.chars()
-                .map( c -> c + amount)                // the character by amount
-                .mapToObj( c -> Character.toChars(c)) // Construct a new Character containing the shifted value
-                .map( c -> String.valueOf(c))         // Convert back to String
-                .collect(joining());                  // Return a String instead of ReferencePipeline
-    }
 
     private static Stream<String> readLinesFromFile(String filename) throws Exception{
         Path p = FileSystems.getDefault().getPath(filename);
         return Files.lines(p);
+    }
+
+    public static String cipherByPassword(String s, int amount){
+        return s.chars()
+                .filter(c -> Character.isAlphabetic(c) || Character.isWhitespace(c)) // filter all values, that are not Characters or Whitespace
+                .map( c -> c + amount)                // shift the character by amount -> e.g. a + 10 = k
+                .mapToObj( c -> Character.toChars(c)) // Construct a new Character containing the shifted value
+                .map(c -> String.valueOf(c))          // Convert back to String
+                .collect(joining());                  // Return a String instead of ReferencePipeline
     }
 
     public static void main(String[] args) throws Exception{
@@ -44,7 +36,6 @@ public class Caesar {
         // get The Stuff going...should be obvious, what it's doing
         readLinesFromFile("resource/quote.txt")
              .map(String::toLowerCase)
-             .map(Caesar::removeAllNonAlphabetics)
              .map(s -> Caesar.cipherByPassword(s, password))
              .forEach(System.out::println);
     }
