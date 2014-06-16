39f7d3377de57a4c256548025a1654a6da028995
Felix Klotzsche <felixklotzsche@gmail.com>
2014-06-16 17:53:38 +0200

    updated importes

--- src/de/klotzsche/playground/Caesar/Caesar.java
+++ src/de/klotzsche/playground/Caesar/Caesar.java
@@ -13,31 +13,38 @@ import static java.util.stream.Collectors.joining;
  */
 public class Caesar {
 
-
     private static Stream<String> readLinesFromFile(String filename) throws Exception{
-        Path p = FileSystems.getDefault().getPath(filename);
+        final Path p = FileSystems.getDefault().getPath(filename);
         return Files.lines(p);
     }
 
     public static String cipherByPassword(String s, int amount){
         return s.chars()
                 .filter(c -> Character.isAlphabetic(c) || Character.isWhitespace(c)) // filter all values, that are not Characters or Whitespace
-                .map( c -> c + amount)                // shift the character by amount -> e.g. a + 10 = k
-                .mapToObj( c -> Character.toChars(c)) // Construct a new Character containing the shifted value
-                .map(c -> String.valueOf(c))          // Convert back to String
+                .map(c -> c + amount)                // shift the character by amount -> e.g. a + 10 = k
+                .mapToObj(Character::toChars)         // Construct a new Character containing the shifted value
+                .map(String::valueOf)                 // Convert back to String
                 .collect(joining());                  // Return a String instead of ReferencePipeline
+    }
+
+    public static void WriteCryptedToFile(String filename, String contents) throws Exception{
+        final Path fullFileName = FileSystems.getDefault().getPath(filename);
+        Files.deleteIfExists(fullFileName);
+        Files.createFile(fullFileName);
+        Files.write(fullFileName, contents.getBytes());
     }
 
     public static void main(String[] args) throws Exception{
         // Just grab the Password to cipher
         System.out.print("Gib das Paswort ein: ");
-        int password = new Scanner(System.in).nextInt();
+        final int password = new Scanner(System.in).nextInt();
 
         // get The Stuff going...should be obvious, what it's doing
-        readLinesFromFile("resource/quote.txt")
-             .map(String::toLowerCase)
-             .map(s -> Caesar.cipherByPassword(s, password))
-             .forEach(System.out::println);
+        final String cipheredContents = readLinesFromFile("resource/quote.txt")
+                .map(String::toLowerCase)
+                .map(s -> Caesar.cipherByPassword(s, password))
+                .collect(joining());
+        Caesar.WriteCryptedToFile("resource/ciphered_quote.txt", cipheredContents);
     }
 
 }
