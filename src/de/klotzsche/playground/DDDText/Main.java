65aa925d2074a22935471b575c3e834e5f8c758b
Felix Klotzsche <felixklotzsche@gmail.com>
2014-06-16 19:15:25 +0200

    cleaned up a bit and changed comments

--- src/de/klotzsche/playground/DDDText/Main.java
+++ src/de/klotzsche/playground/DDDText/Main.java
@@ -1,13 +1,7 @@
 package de.klotzsche.playground.DDDText;
 
 import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
-
-import static java.util.stream.Collectors.joining;
-import static java.util.stream.Collectors.toList;
-import static java.util.stream.IntStream.range;
-import static java.util.stream.IntStream.rangeClosed;
 
 /**
  * Created by Felix Klotzsche on 16.06.2014.
@@ -15,13 +9,13 @@ import static java.util.stream.IntStream.rangeClosed;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Gib deinen Text ein: ");
+        System.out.print("Gib deinen Text ein: ");
+
         new Scanner(System.in).nextLine() // Grab text from command line
                 .toLowerCase()            // lowercase it
                 .chars()                  // turn it into a char[]
                 .mapToObj(i -> Letters.letters[i - 97]) // subtract from each char 97 so 'a' becomes 0 and grab the according 3D Letter
-                .flatMap( c -> Arrays.stream(c)) // make it flat into a list of string
-                .forEach(System.out::println); // print each value
-        // best things of all...no mutable state
+                .flatMap(c -> Arrays.stream(c)) // Turn all nested arrays into one big datastructure
+                .forEach(System.out::println); // print each line
     }
 }
